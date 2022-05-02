68b3ab44cb4499b7d655fded83b0b96ed9129bf4
ROCKTAKEY <rocktakey@gmail.com>
2022-05-03 02:08:13 +0900

    Add tailscale

---
+++ roquix/packages/tailscale.scm
@@ -0,0 +1,28 @@
+(define-module
+  (roquix packages tailscale)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses)  #:prefix license:)
+  #:use-module (guix download)
+  #:use-module (nonguix build-system binary))
+
+(define-public tailscale
+  (package
+   (name "tailscale")
+   (version "1.24.2")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "https://pkgs.tailscale.com/stable/tailscale_"
+                                version "_amd64.tgz"))
+            (sha256
+             (base32
+              "1b697g694vigzmv5q48l1d3pjc9l5gwzazggnfi7z9prb9cvlnx2"))))
+   (build-system binary-build-system)
+   (arguments '(#:install-plan
+                '(("tailscale" "/bin/")
+                  ("tailscaled" "/bin/"))))
+   (home-page "https://tailscale.com/")
+   (synopsis "Tailscale")
+   (description
+    "A secure network that just works
+Zero config VPN. Installs on any device in minutes, manages firewall rules for you, and works from anywhere.")
+   (license license:bsd-3)))
