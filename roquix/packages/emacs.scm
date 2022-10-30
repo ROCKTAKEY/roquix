3c00671bfdd11148075248dc45155d7d4d264148
ROCKTAKEY <rocktakey@gmail.com>
2022-10-30 19:08:30 +0900

    Add `emacs-cask`

--- roquix/packages/emacs.scm
+++ roquix/packages/emacs.scm
@@ -38,3 +38,36 @@
      "Modern Elisp package development system.
 Keg is 100% Elisp project and it developed as alternative to Cask.")
     (license license:gpl3+)))
+
+(define-public emacs-cask
+  (let ((commit "2fd12b3aef435bcbdd31e64fbdfbbe69e7bcc65f")
+        (revision "0"))
+   (package
+    (name "emacs-cask")
+    (version (git-version "0.8.8" revision commit))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/cask/cask")
+             (commit commit)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "1m3y2icmj9l0bix66i6gmfsn0j934qfv5wi5p4sw6086n6mc5wrr"))))
+    (build-system copy-build-system)
+    (arguments '(#:install-plan
+                 '(("." "src/cask"))
+                 #:phases
+                 (modify-phases %standard-phases
+                   (add-after 'install 'install-bin
+                     (lambda* (#:key outputs #:allow-other-keys)
+                       (mkdir-p (string-append (assoc-ref outputs "out") "/bin"))
+                       (symlink (string-append (assoc-ref outputs "out") "/src/cask/bin/cask")
+                                (string-append (assoc-ref outputs "out") "/bin/cask")))))))
+    (propagated-inputs (list emacs))
+    (home-page "https://github.com/cask/cask")
+    (synopsis "Project management tool for Emacs")
+    (description
+     "Project management tool for Emacs.")
+    (license license:gpl3+))))
