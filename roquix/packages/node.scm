1b7b32a522f69be26efe9a31bcd9c223221dc020
ROCKTAKEY <rocktakey@gmail.com>
2023-06-23 19:00:28 +0900

    Update node-latest

--- roquix/packages/node.scm
+++ roquix/packages/node.scm
@@ -9,29 +9,27 @@
 
 (define-public node-latest
   (package
-   (name "node")
-   (version "19.0.0")
-   (source (origin
-            (method url-fetch)
-            (uri (string-append "https://nodejs.org/dist/v"
-                                version "/node-v" version "-linux-x64.tar.xz"))
-            (sha256
-             (base32
-              "163n1432b2m6q9jlrnr1l6chq99mwnmxny378zwz1px79gys0vx1"))))
-   (build-system binary-build-system)
-   (arguments '(#:install-plan
-                '(("bin" "bin")
-                  ("include" "include")
-                  ("lib" "lib")
-                  ("share" "share"))
-                #:patchelf-plan
-                `(("bin/node" ("gcc:lib" "gcc-toolchain")))
-                #:strip-binaries? #f))
-   (inputs
-    `(("gcc:lib" ,gcc "lib")
-      ("gcc-toolchain" ,gcc-toolchain)))
-   (home-page "https://nodejs.org/")
-   (synopsis "nodejs")
-   (description
-    "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.")
-   (license license:expat)))
+    (name "node")
+    (version "20.3.1")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://nodejs.org/dist/v" version
+                                  "/node-v" version "-linux-x64.tar.xz"))
+              (sha256
+               (base32
+                "0d03qm8d2cbqzdy45r3qdsli617xxv5jqa5nm0l0371zfqsl9yd9"))))
+    (build-system binary-build-system)
+    (arguments
+     `(#:install-plan '(("bin" "bin")
+                        ("include" "include")
+                        ("lib" "lib")
+                        ("share" "share"))
+       #:patchelf-plan `(("bin/node" ("gcc" "gcc-toolchain")))
+       #:strip-binaries? #f))
+    (inputs `((,gcc "lib")
+              ,gcc-toolchain))
+    (home-page "https://nodejs.org/")
+    (synopsis "nodejs")
+    (description
+     "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.")
+    (license license:expat)))
