5685fa4206a3a4a881419d7745ff28f201612b7a
ROCKTAKEY <rocktakey@gmail.com>
2023-07-31 03:06:10 +0900

    Add header-only library of argumentum

--- roquix/packages/cpp.scm
+++ roquix/packages/cpp.scm
@@ -5,7 +5,8 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix gexp)
-  #:use-module (guix build-system cmake))
+  #:use-module (guix build-system cmake)
+  #:use-module (gnu packages check))
 
 (define-public argh
   (package
@@ -34,3 +35,31 @@
      "A minimalist argument handler for C++.")
     (home-page "https://github.com/adishavit/argh")
     (license license:bsd-3)))
+
+(define-public argumentum
+  (package
+   (name "argumentum")
+   (version "0.3.2")
+   (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/mmahnic/argumentum")
+                  (commit (string-append "v" version))))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "0fx73mj2hk4h5kviy8kis5330rkrryf8p8jd2iskyh0vs9vnanac"))))
+   (build-system cmake-build-system)
+   (arguments
+    '(#:configure-flags '("-DARGUMENTUM_INSTALL_HEADERONLY=ON"
+                          "-DARGUMENTUM_BUILD_TESTS=ON")))
+   (native-inputs (list googletest))
+   (home-page "https://github.com/mmahnic/argumentum")
+   (synopsis "C++ command line parsing library")
+   (description
+    "Argumentum is a C++ library for writing command-line program interfaces.
+The parameters that a program supports are registered in an instance of arument_parser,
+the main library class. argument_parser processes the input arguments,
+checks that they are valid and converts them to C++ variables.
+It also generates help and usage messages when requested.")
+   (license license:mpl2.0)))
