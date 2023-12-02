d046ce5d1d5f52d7df0e84b61dd3a552df74b230
ROCKTAKEY <rocktakey@gmail.com>
2023-12-02 11:46:27 +0900

    Update gitlab cli

--- roquix/packages/glab.scm
+++ roquix/packages/glab.scm
@@ -5,26 +5,25 @@
   #:use-module (guix download)
   #:use-module (nonguix build-system binary))
 
-(define-public go-github-com-profclems-glab
+(define-public go-gitlab-com-gitlab-org-cli
   (package
-   (name "go-github-com-profclems-glab")
-   (version "1.22.0")
+   (name "go-gitlab-com-gitlab-org-cli")
+   (version "1.35.0")
    (source (origin
             (method url-fetch)
-            (uri (string-append "https://github.com/profclems/glab/releases/download/v"
-                                version "/glab_" version "_Linux_x86_64.tar.gz"))
+            (uri (string-append "https://gitlab.com/gitlab-org/cli/-/releases/v"
+                                version "/downloads/glab_" version "_Linux_x86_64.tar.gz"))
             (sha256
              (base32
-              "0pwa4kakka88cnbzcg3bcfgpd0grzngdsp9ik4475mwccjaayw3x"))))
+              "19n0wg5qjg6ixsk3ysjkx2dl432riijyaazs8vwyjngbx3813fn9"))))
    (build-system binary-build-system)
    (arguments '(#:install-plan
                 '(("glab" "/bin/"))))
-   (home-page "https://github.com/profclems/glab")
-   (synopsis "GLab")
+   (home-page "https://gitlab.com/gitlab-org/cli")
+   (synopsis "A GitLab CLI tool bringing GitLab to your command line")
    (description
-    "GLab is an open source GitLab CLI tool bringing GitLab to your terminal next to
-where you are already working with @code{git} and your code without switching
-between windows and browser tabs.  Work with issues, merge requests,
-@strong{watch running pipelines directly from your CLI} among other features.
-Inspired by @url{https://github.com/cli/cli,gh}, the official GitHub CLI tool.")
+    "GLab is an open source GitLab CLI tool bringing GitLab to your terminal
+next to where you are already working with @code{git} and your code without switching
+between windows and browser tabs. Work with issues, merge requests, watch
+running pipelines directly from your CLI among other features.")
    (license license:expat)))
