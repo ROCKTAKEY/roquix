2767321a92fa31994ab04e976e65ce73c609586c
ROCKTAKEY <rocktakey@gmail.com>
2024-10-15 00:07:35 +0900

    Update nodejs

--- roquix/packages/node.scm
+++ roquix/packages/node.scm
@@ -10,14 +10,14 @@
 (define-public node-latest
   (package
     (name "node")
-    (version "22.6.0")
+    (version "22.9.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://nodejs.org/dist/v" version
                                   "/node-v" version "-linux-x64.tar.xz"))
               (sha256
                (base32
-                "00spngkl27pcw4c87vanc87dg9awbdagf98v7sxhjcn3xlwybfxc"))))
+                "0hk1c847aiv49j6s34j103m447zn5c1vvwblh8nwjhxb47pykyhv"))))
     (build-system binary-build-system)
     (arguments
      `(#:install-plan '(("bin" "bin")
@@ -25,16 +25,7 @@
                         ("lib" "lib")
                         ("share" "share"))
        #:patchelf-plan `(("bin/node" ("gcc" "gcc-toolchain")))
-       #:strip-binaries? #f
-       #:phases
-       (modify-phases %standard-phases
-         (add-after 'install 'install-corepack
-           (lambda* (#:key outputs #:allow-other-keys)
-             ;; FIXME: Cannot install npm because installing npm with corepack causes segmentation fault
-             (invoke (string-append (assoc-ref outputs "out") "/bin/node")
-                     (string-append (assoc-ref outputs "out") "/bin/corepack")
-                     "enable"
-                     "--install-directory" (string-append (assoc-ref outputs "out") "/bin")))))))
+       #:strip-binaries? #f))
     (inputs `((,gcc "lib")
               ,gcc-toolchain))
     (home-page "https://nodejs.org/")
