e580afcc881672051612a138c01a1aba4e1b37fd
ROCKTAKEY <rocktakey@gmail.com>
2024-12-02 01:52:04 +0900

    Fix dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ((nil
   (geiser-guile-binary . ("guix" "repl"))
   (eval . (setq geiser-guile-load-path
-                (list (expand-file-name "." (locate-dominating-file default-directory ".dir-locals.el")))))))
+                (cons
+                 (expand-file-name "." (locate-dominating-file default-directory ".dir-locals.el"))
+                 geiser-guile-load-path)))))
