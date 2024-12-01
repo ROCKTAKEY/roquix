b41186e667db5b761daed90749e1e8a1f332be9c
ROCKTAKEY <rocktakey@gmail.com>
2024-12-02 01:51:55 +0900

    Add lua-language-server

--- roquix/packages/lsp.scm
+++ roquix/packages/lsp.scm
@@ -2,18 +2,22 @@
   #:use-module (guix packages)
   #:use-module ((guix licenses)
                 #:prefix license:)
-  #:use-module (gnu packages base)
   #:use-module (guix download)
-  #:use-module (gnu packages gcc)
+  #:use-module (guix utils)
   #:use-module (guix build-system pyproject)
   #:use-module (guix build-system python)
   #:use-module (guix git-download)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages base)
   #:use-module (gnu packages openstack)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages python-check)
   #:use-module (gnu packages check)
   #:use-module (gnu packages python-build)
-  #:use-module (guix utils))
+  #:use-module (gnu packages lua)
+  #:use-module (gnu packages ninja)
+  #:use-module (guix build-system gnu)
+  #:use-module (nonguix build-system binary))
 
 (define-public python-pygls
   (package
@@ -66,4 +70,45 @@
     (home-page "https://github.com/regen100/cmake-language-server")
     (synopsis "CMake LSP Implementation")
     (description "CMake LSP Implementation.")
+    (license license:expat)))
+
+(define-public lua-language-server
+  (package
+    (name "lua-language-server")
+    (version "3.13.2")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/LuaLS/lua-language-server/")
+             (commit version)
+             (recursive? #t)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1sgfn2r3gmh0h5saks5g8qmmm3nra77xw0k87c2kcps42c82bqi4"))))
+    (build-system gnu-build-system)
+    (arguments
+     '(;; Test seems to be run in build phase.
+        #:tests? #f
+       #:phases (modify-phases %standard-phases
+                  (delete 'configure)
+                  (add-before 'build 'build-luamake
+                    (lambda _
+                      (with-directory-excursion "3rd/luamake"
+                        (invoke "./compile/build.sh"))))
+                  (replace 'build
+                    (lambda _
+                      (invoke "3rd/luamake/luamake" "rebuild")))
+                  (replace 'install
+                    (lambda* (#:key outputs #:allow-other-keys)
+                      (mkdir-p (string-append (assoc-ref outputs "out") "/bin"))
+                      (copy-file "./build//bin/lua-language-server"
+                                 (string-append (assoc-ref outputs "out")
+                                                "/bin/lua-language-server")))))))
+    (native-inputs (list ninja lua glibc))
+    (home-page "https://luals.github.io/")
+    (synopsis
+     "Language server that offers Lua language support - programmed in Lua")
+    (description
+     "A language server that offers Lua language support - programmed in Lua")
     (license license:expat)))
