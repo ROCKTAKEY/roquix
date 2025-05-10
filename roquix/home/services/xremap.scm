16abd510152854059d3089bfb827bf8efdc4a9f2
ROCKTAKEY <rocktakey@gmail.com>
2025-05-11 04:13:37 +0900

    Update xremap home service

--- roquix/home/services/xremap.scm
+++ roquix/home/services/xremap.scm
@@ -7,14 +7,21 @@
   #:use-module (gnu packages rust-apps)
   #:use-module (gnu packages admin)
   #:use-module (guix gexp)
+  #:use-module (guix packages)
   #:export (home-xremap-configuration
             home-xremap-service-type))
 
 
 (define-configuration/no-serialization home-xremap-configuration
+  (xremap
+   (package xremap-x11)
+   "The package providing xremap.")
   (config-file
    (file-like)
-   "Path to config.yml file for xremap."))
+   "Path to config.yml file for xremap.")
+  (debug
+   (boolean #f)
+   "Whether to output debug log."))
 
 (define (home-xremap-services config)
   (list
@@ -22,10 +29,18 @@
     (provision '(xremap))
     (modules '((shepherd support)))
     (start #~(make-forkexec-constructor
-              (list #$(file-append xremap-x11 "/bin/xremap")
+              (list #$(file-append (home-xremap-configuration-xremap config) "/bin/xremap")
                     "--watch=device"
                     #$(home-xremap-configuration-config-file config))
-              #:log-file (string-append %user-log-dir "/xremap.log")))
+              #:log-file (string-append %user-log-dir "/xremap.log")
+              #:environment-variables
+              (append
+               '("DISPLAY=:0"
+                 "XAUTHORITY=/home/rocktakey/.Xauthority")
+               (if #$(home-xremap-configuration-debug config)
+                   '("RUST_LOG=debug")
+                   '())
+               (default-environment-variables))))
     (stop #~(make-kill-destructor))
     (documentation "Run @code{xremap} with the configuration file."))))
 
