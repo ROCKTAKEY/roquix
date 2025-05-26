84e873dcfc00dfeddb891fa44f3dbd7abe0c184a
ROCKTAKEY <rocktakey@gmail.com>
2025-05-26 22:47:46 +0900

    Remove xremap home service

--- roquix/home/services/xremap.scm
+++ roquix/home/services/xremap.scm
@@ -11,44 +11,46 @@
   #:export (home-xremap-configuration
             home-xremap-service-type))
 
+;; FIXME: xremap needs the environment variable XAUTHORITY, but it can be accessed only from login shell,
+;;   while shepherd does not run login shell.
 
-(define-configuration/no-serialization home-xremap-configuration
-  (xremap
-   (package xremap-x11)
-   "The package providing xremap.")
-  (config-file
-   (file-like)
-   "Path to config.yml file for xremap.")
-  (debug
-   (boolean #f)
-   "Whether to output debug log."))
+;; (define-configuration/no-serialization home-xremap-configuration
+;;   (xremap
+;;    (package xremap-x11)
+;;    "The package providing xremap.")
+;;   (config-file
+;;    (file-like)
+;;    "Path to config.yml file for xremap.")
+;;   (debug
+;;    (boolean #f)
+;;    "Whether to output debug log."))
 
-(define (home-xremap-services config)
-  (list
-   (shepherd-service
-    (provision '(xremap))
-    (modules '((shepherd support)))
-    (start #~(make-forkexec-constructor
-              (list #$(file-append (home-xremap-configuration-xremap config) "/bin/xremap")
-                    "--watch=device"
-                    #$(home-xremap-configuration-config-file config))
-              #:log-file (string-append %user-log-dir "/xremap.log")
-              #:environment-variables
-              (append
-               '("DISPLAY=:0"
-                 "XAUTHORITY=/home/rocktakey/.Xauthority")
-               (if #$(home-xremap-configuration-debug config)
-                   '("RUST_LOG=debug")
-                   '())
-               (default-environment-variables))))
-    (stop #~(make-kill-destructor))
-    (documentation "Run @code{xremap} with the configuration file."))))
+;; (define (home-xremap-services config)
+;;   (list
+;;    (shepherd-service
+;;     (provision '(xremap))
+;;     (modules '((shepherd support)))
+;;     (start #~(make-forkexec-constructor
+;;               (list #$(file-append (home-xremap-configuration-xremap config) "/bin/xremap")
+;;                     "--watch=device"
+;;                     #$(home-xremap-configuration-config-file config))
+;;               #:log-file (string-append %user-log-dir "/xremap.log")
+;;               #:environment-variables
+;;               (append
+;;                '("DISPLAY=:0"
+;;                  "XAUTHORITY=/home/${HOME}/.Xauthority")
+;;                (if #$(home-xremap-configuration-debug config)
+;;                    '("RUST_LOG=debug")
+;;                    '())
+;;                (default-environment-variables))))
+;;     (stop #~(make-kill-destructor))
+;;     (documentation "Run @code{xremap} with the configuration file."))))
 
-(define home-xremap-service-type
-  (service-type
-   (name 'home-xremap)
-   (extensions
-    (list
-     (service-extension home-shepherd-service-type
-                        home-xremap-services)))
-   (description "Run the @code{xremap} utility to modify keymaps.")))
+;; (define home-xremap-service-type
+;;   (service-type
+;;    (name 'home-xremap)
+;;    (extensions
+;;     (list
+;;      (service-extension home-shepherd-service-type
+;;                         home-xremap-services)))
+;;    (description "Run the @code{xremap} utility to modify keymaps.")))
