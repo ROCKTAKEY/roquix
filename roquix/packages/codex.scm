d2fd96659a237a8b11ac5d018e849d66acdeb390
ROCKTAKEY <rocktakey@gmail.com>
2026-02-08 19:58:25 +0900

    Update to 1.91.0

--- roquix/packages/codex.scm
+++ roquix/packages/codex.scm
@@ -31,7 +31,7 @@
        (sha256
         (base32 "1miwhlq2l83x7mnn329zqs5f4xg0lkwfd8hf1y3jd1sy0zkkxy4v"))))
     ;; TODO: Use official rust-1.89.0 when the official guix channel is updated
-    (build-system (make-cargo-build-system "1.90.0"))
+    (build-system (make-cargo-build-system "1.91.0"))
     (inputs (cons* ;; clang-toolchain openssl `(,zstd "lib") gcc-toolchain libunwind
                    (cargo-inputs 'codex
                                  #:module '(roquix packages rust-crates))))
