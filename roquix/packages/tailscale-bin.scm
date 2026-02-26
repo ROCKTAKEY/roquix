c9fb2c508e0ff62284cc9f85bb9aba8bbd025c02
ROCKTAKEY <rocktakey@gmail.com>
2026-02-27 00:46:13 +0900

    Add non-binary tailscale and tailscaled

---
+++ roquix/packages/tailscale-bin.scm
@@ -0,0 +1,31 @@
+(define-module
+  (roquix packages tailscale-bin)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses)  #:prefix license:)
+  #:use-module (guix download)
+  #:use-module (gnu packages linux)
+  #:use-module (nonguix build-system binary))
+
+(define-public tailscale-bin
+  (package
+   (name "tailscale-bin")
+   (version "1.94.1")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append "https://pkgs.tailscale.com/stable/tailscale_"
+                                version "_amd64.tgz"))
+            (sha256
+             (base32
+              "0726s9v2lfq6fi378g9cci87qq1x73s0z0dfg9j98byjssy0gnc4"))))
+   (build-system binary-build-system)
+   (arguments '(#:install-plan
+                '(("tailscale" "bin/")
+                  ("tailscaled" "bin/"))))
+   (home-page "https://tailscale.com/")
+   (synopsis "Tailscale")
+   (propagated-inputs
+    (list iptables))
+   (description
+    "A secure network that just works
+Zero config VPN. Installs on any device in minutes, manages firewall rules for you, and works from anywhere.")
+   (license license:bsd-3)))
