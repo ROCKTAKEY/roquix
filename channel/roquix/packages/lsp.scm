bd6496f1a253096816c54ed741da74662f3507eb
ROCKTAKEY <rocktakey@gmail.com>
2026-05-08 00:39:24 +0900

    Add typescript-language-server

--- channel/roquix/packages/lsp.scm
+++ channel/roquix/packages/lsp.scm
@@ -3,11 +3,14 @@
   #:use-module ((guix licenses)
                 #:prefix license:)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages node-xyz)
   #:use-module (guix download)
   #:use-module (gnu packages gcc)
+  #:use-module (guix build-system node)
   #:use-module (guix build-system pyproject)
   #:use-module (guix build-system python)
   #:use-module (guix git-download)
+  #:use-module (guix gexp)
   #:use-module (gnu packages openstack)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages python-check)
@@ -67,3 +70,51 @@
     (synopsis "CMake LSP Implementation")
     (description "CMake LSP Implementation.")
     (license license:expat)))
+
+(define-public typescript-language-server
+  (package
+    (name "typescript-language-server")
+    (version "5.1.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://registry.npmjs.org/typescript-language-server"
+                           "/-/typescript-language-server-" version ".tgz"))
+       (sha256
+        (base32
+         "031iglrwqb5cyriixhpg21f1lxd2ya2rshy9vi5184p7gzpcmjfj"))))
+    (build-system node-build-system)
+    (arguments
+     (list
+      #:tests? #f
+      #:phases
+      #~(modify-phases %standard-phases
+          (delete 'build)
+          (add-after 'patch-dependencies 'keep-only-typescript-dependency
+            (lambda _
+              (modify-json
+               (lambda (package-json)
+                 (let* ((dev-dependencies
+                         (assoc-ref package-json "devDependencies"))
+                        (typescript
+                         (assoc-ref dev-dependencies "typescript"))
+                        (package-json
+                         (filter
+                          (lambda (field)
+                            (not (member (car field)
+                                         '("devDependencies"
+                                           "eslintIgnore"
+                                           "husky"
+                                           "resolutions"))))
+                          package-json)))
+                   (assoc-set! package-json
+                               "dependencies"
+                               `(("typescript" . ,typescript)))))))))))
+    (inputs (list node-typescript))
+    (home-page
+     "https://github.com/typescript-language-server/typescript-language-server")
+    (synopsis "TypeScript and JavaScript language server")
+    (description
+     "TypeScript Language Server provides a Language Server Protocol
+implementation for TypeScript and JavaScript using TypeScript's tsserver.")
+    (license (list license:asl2.0 license:expat))))
